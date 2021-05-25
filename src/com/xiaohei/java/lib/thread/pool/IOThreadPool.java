079ab47650acef3b1a173b4423abe421d66d8dcf
liuhuiliang <liuhuiliang@bianfeng.com>
2021-05-25 14:19:05 +0800

    修改线程数量和队列数量

--- src/com/xiaohei/java/lib/thread/pool/IOThreadPool.java
+++ src/com/xiaohei/java/lib/thread/pool/IOThreadPool.java
@@ -6,8 +6,8 @@ import java.util.Collection;
 import java.util.concurrent.*;
 
 public class IOThreadPool extends ThreadPoolExecutor {
-    private final static int QUEUE_SIZE = Integer.MAX_VALUE;
-    private static final int MAX_THREAD=500;
+    private final static int QUEUE_SIZE = 128;
+    private static final int MAX_THREAD=ThreadConstant.MAXINUM_POOL_XIZE+1;
 
     public IOThreadPool() {
         this(MAX_THREAD);
